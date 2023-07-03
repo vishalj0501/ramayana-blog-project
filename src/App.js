de1ba695d866146f22374fe32942dcca205f7124
vishalj0501 <jvishal0501@gmail.com>
2023-07-03 22:37:33 +0530

    fix:updated the route

--- src/App.js
+++ src/App.js
@@ -48,8 +48,8 @@ function App() {
           <div className="bg-orange-300 dark:text-white min-h-screen pb-10">
             <Navbar isDark={isDark} setIsDark={setIsDark} />
             <Routes>
-              <Route exact path="/blog" element={<Home />} />
-              <Route exact path="/blog/:id" element={<Blog ctx={ctx} />} />
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/:id" element={<Blog ctx={ctx} />} />
             </Routes>
           </div>
         </BrowserRouter>
